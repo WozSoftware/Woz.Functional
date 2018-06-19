be9f01eb6495ebee5778669c5cdce4b688d239a4
WozSoftware <warrenleggatt@gmail.com>
2018-06-19 21:16:03 +1200

    Initial Maybe cut + tests

---
+++ Test.Woz.Functional/Monads/MaybeTests.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using Woz.Functional.Monads;
+using Xunit;
+
+namespace Test.Woz.Functional.Monads
+{
+    public class MaybeTests
+    {
+        [Fact]
+        public void Construction()
+        {
+            Assert.Equal(5, 5.ToSome().Value);
+
+            Assert.Equal(5, 5.ToMaybe().Value);
+
+            Assert.Equal(Maybe<int>.None.HasValue, ((int?)null).ToMaybe().HasValue);
+        }
+
+        [Fact]
+        public void SomeState()
+        {
+            var maybe = 5.ToSome();
+            Assert.Equal(5, maybe.Value);
+            Assert.True(maybe.HasValue);
+        }
+
+        [Fact]
+        public void NoneState()
+        {
+            Assert.False(Maybe<int>.None.HasValue);
+            Assert.Throws<InvalidOperationException>(() => Maybe<int>.None.Value);
+        }
+
+        [Fact]
+        public void SelectManyBothSome()
+        {
+            var result =
+                from a in 5.ToSome()
+                from b in 3.ToSome()
+                select a + b;
+
+            Assert.Equal(8, result.Value);
+        }
+
+        [Fact]
+        void SelectManyFirstNone()
+        {
+            var result =
+                from a in Maybe<int>.None
+                from b in 3.ToSome()
+                select a + b;
+
+            Assert.False(result.HasValue);
+        }
+
+        [Fact]
+        void SelectManySecondNone()
+        {
+            var result =
+                from a in 3.ToSome()
+                from b in Maybe<int>.None
+                select a + b;
+
+            Assert.False(result.HasValue);
+        }
+
+        [Fact]
+        public void Select()
+        {
+            Assert.Equal(6, 5.ToSome().Select(x => x + 1).Value);
+            Assert.False(Maybe<int>.None.Select(x => x + 1).HasValue);
+        }
+
+        [Fact]
+        public void Where()
+        {
+            Assert.Equal(5, 5.ToSome().Where(_ => true).Value);
+            Assert.False(5.ToSome().Where(_ => false).HasValue);
+            Assert.False(Maybe<int>.None.Where(_ => true).HasValue);
+            Assert.False(Maybe<int>.None.Where(_ => false).HasValue);
+        }
+
+        [Fact]
+        public void RecoverValue()
+        {
+            Assert.Equal(5, 5.ToSome().Recover(7).Value);
+            Assert.Equal(7, Maybe<int>.None.Recover(7).Value);
+        }
+    }
+}
---
+++ Woz.Functional/Monads/Maybe.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace Woz.Functional.Monads
+{
+    public struct Maybe<T> 
+    {
+        private readonly (T, bool) _contents;
+
+        public static Maybe<T> None => new Maybe<T>((default(T), false));
+
+        //internal Maybe(T value) => _contents = (value, true);
+
+        internal Maybe((T, bool) contents) => _contents = contents;
+
+        public bool HasValue => _contents.Item2;
+
+        public T Value
+        {
+            get
+            {
+                if (!HasValue)
+                {
+                    throw new InvalidOperationException(
+                        $"{nameof(Maybe<T>)} does not have a value.");
+                }
+                return _contents.Item1;
+            }
+        }
+    }
+
+    public static class Maybe 
+    {
+        #region Construction
+        public static Maybe<T> ToSome<T>(this T value) => new Maybe<T>((value, true));
+        public static Maybe<T> ToMaybe<T>(this T value) => value != null ? new Maybe<T>((value, true)) : Maybe<T>.None;
+        #endregion
+
+        #region LINQ=Map/Flatmap
+        public static Maybe<TR> SelectMany<T, TR>(
+             this Maybe<T> maybe, Func<T, Maybe<TR>> selector)
+            => maybe.HasValue
+                ? selector(maybe.Value)
+                : Maybe<TR>.None;
+
+        public static Maybe<TR> SelectMany<T1, T2, TR>(
+            this Maybe<T1> maybe,
+            Func<T1, Maybe<T2>> selector,
+            Func<T1, T2, TR> projection)
+            => maybe.HasValue
+                ? selector(maybe.Value).Select(value => projection(maybe.Value, value))
+                : Maybe<TR>.None;
+
+        public static Maybe<TR> Select<T, TR>(this Maybe<T> maybe, Func<T, TR> selector)
+            => maybe.SelectMany(x => selector(x).ToMaybe());
+
+        public static Maybe<T> Where<T>(
+            this Maybe<T> maybe, Func<T, bool> predicate)
+            => maybe.SelectMany(value => predicate(value) ? value.ToSome() : Maybe<T>.None);
+        #endregion
+
+        #region Recovery
+        public static Maybe<T> Recover<T>(
+            this Maybe<T> maybe, T defaultValue)
+            => maybe.HasValue ? maybe : defaultValue.ToMaybe();
+
+        public static Maybe<T> Recover<T>(
+            this Maybe<T> maybe, Func<T> defaultFactory)
+            => maybe.HasValue ? maybe : defaultFactory().ToMaybe();
+        #endregion
+
+
+    }
+}
