f434d4fa8f7f440fbbad3982ae0aff4ebf584c11
WozSoftware <warrenleggatt@gmail.com>
2018-07-03 20:44:29 +1200

    State monad added

---
+++ Test.Woz.Functional/Monads/StateTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Woz.Functional.Monads;
+using Xunit;
+
+namespace Test.Woz.Functional.Monads
+{
+    public class StateTests
+    {
+        [Fact]
+        public void Construction()
+        {
+            const string stateInstance = "SomeState";
+            const int value = 5;
+
+            var state = value.ToState<string, int>();
+            var result = state(stateInstance);
+
+            Assert.Equal(stateInstance, result.Item1);
+            Assert.Equal(value, result.Item2);
+        }
+
+        [Fact]
+        public void SelectManySimple()
+        {
+            const string stateInstance = "SomeState";
+            const string stateUpdate = "Updated";
+            const int value = 5;
+
+            var state = value.ToState<string, int>().SelectMany<string, int, int>(v => s => (s + stateUpdate, v + 1));
+
+            Assert.Equal(stateInstance + stateUpdate, state(stateInstance).Item1);
+            Assert.Equal(value + 1, state(stateInstance).Item2);
+        }
+
+        [Fact]
+        public void SelectManyFull()
+        {
+            const string stateInstance = "SomeState";
+
+            var state =
+                from a in 5.ToState<string, int>()
+                from b in 3.ToState<string, int>()
+                select a + b;
+
+            Assert.Equal(stateInstance, state(stateInstance).Item1);
+            Assert.Equal(8, state(stateInstance).Item2);
+        }
+
+        [Fact]
+        public void Select()
+        {
+            const string stateInstance = "SomeState";
+            const int value = 5;
+
+            var state = value.ToState<string, int>().Select(v => v + 1);
+
+            Assert.Equal(stateInstance, state(stateInstance).Item1);
+            Assert.Equal(value + 1, state(stateInstance).Item2);
+        }
+    }
+}
---
+++ Woz.Functional/Monads/State.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Woz.Functional.Monads
+{
+    public delegate (TS, TV) State<TS, TV>(TS state);
+
+    public static class StateExtensions
+    {
+        #region Construction
+        public static State<TS, TV> ToState<TS, TV>(this TV value) => state => (state, value);
+        #endregion
+
+        #region LINQ=Map/Flatmap
+        public static State<TS, TR> SelectMany<TS, T, TR>(
+            this State<TS, T> source, Func<T, State<TS, TR>> selector) 
+            => state =>
+            {
+                var sourceResult = source(state);
+                return selector(sourceResult.Item2)(sourceResult.Item1);
+            };
+
+        public static State<TS, TR> SelectMany<TS, T1, T2, TR>(
+            this State<TS, T1> source, Func<T1, State<TS, T2>> selector, Func<T1, T2, TR> projection)
+            => source.SelectMany(
+                value1 => selector(value1).SelectMany<TS, T2, TR>(
+                    value2 => state => (state, projection(value1, value2))));
+
+        public static State<TS, TR> Select<TS, T, TR>(
+            this State<TS, T> source, Func<T, TR> selector)
+            => source.SelectMany<TS, T, TR>(value => state => (state, selector(value)));
+        #endregion
+    }
+}
